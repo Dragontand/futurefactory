5010d07c3238e84213ea3bf1637ad0e44ef3e31d
Dragontand <rafael.kuylaars2@gmail.com>
2026-03-31 16:27:00 +0200

    Updated ModuleObserver
    Updated ModuleObserver

--- app/Observers/ModuleObserver.php
+++ app/Observers/ModuleObserver.php
@@ -8,15 +8,20 @@ class ModuleObserver
 {
     public function created(Module $module) : void
     {
+        $subtTypCount = (int) $module->chassis()->exists()  
+            + (int) $module->propulsion()->exists()  
+            + (int) $module->wheel()->exists() 
+            + (int)$module->steeringWheel()->exists()
+            + (int)$module->chair()->exists();
         // To make sure the Module model is abstract wihtout initiating it as abstract.
-        if (!$module->chassis()->exists() && 
-            !$module->propulsion()->exists() && 
-            !$module->wheel()->exists() &&
-            !$module->steeringWheel()->exists() &&
-            !$module->chair()->exists()
-        )   {
+        if ($subtTypCount === 0)   {
             $module->delete();
             throw new \LogicException('A module cannot exist without a subtype.');
+        }
+        // And to enforce that one Module is one subtype.
+        if ($subtTypCount > 1)   {
+            $module->delete();
+            throw new \LogicException('A module cannot have more than one subtype.');
         }
     }
 }
